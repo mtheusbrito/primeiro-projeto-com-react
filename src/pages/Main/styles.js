40aa285295738f52ae06a5f0b0fb0847a2f4d23e
matheus <matheusbritodasilva@gmail.com>
2019-10-21 14:25:44 -0300

    Adicionando repositórios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Title = styled.h1`
     /* font-size: 24px;
@@ -40,10 +40,20 @@ export const Form = styled.form`
         font-size: 16px;
     }
 `;
-export const SubmitButton = styled.button.attrs({
-    type: 'submit',
 
-})`
+const rotate = keyframes`
+from {
+    transform:rotate(0deg);
+
+}
+to{
+transform:rotate(360deg);
+
+}`;
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading
+}))`
     background: #7159c1;
     border: 0;
     padding: 0 15px;
@@ -52,4 +62,16 @@ export const SubmitButton = styled.button.attrs({
     display: flex;
     justify-content: center;
     align-items: center;
+    &[disabled] {
+        cursor: not-allowed;
+        opacity: 0.6;
+    }
+
+    ${props =>
+        props.loading &&
+        css`
+            svg {
+                animation: ${rotate} 2s linear infinite;
+            }
+        `}
 `;
