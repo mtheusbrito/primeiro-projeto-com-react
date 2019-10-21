1d3949e1f68df1fd14ee6b8bcd593f2351ab7f4b
matheus <matheusbritodasilva@gmail.com>
2019-10-21 16:58:44 -0300

    Exibindo repositorio

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { from } from 'rxjs';
 import api from '../../services/api';
+import { Loading, Owner } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
     static propTypes = {
@@ -44,7 +48,22 @@ export default class Repository extends Component {
 
     render() {
         const { repository, issues, loading } = this.state;
+        if (loading) {
+            return <Loading>Carregando</Loading>;
+        }
 
-        return <div />;
+        return (
+            <Container>
+                <Owner>
+                    <Link to="/"> Voltar aos repositórios</Link>
+                    <img
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
+                    />
+                    <h1>{repository.name}</h1>
+                    <p>{repository.description}</p>
+                </Owner>
+            </Container>
+        );
     }
 }
