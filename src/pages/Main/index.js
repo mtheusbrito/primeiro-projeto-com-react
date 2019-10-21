78a3a587fab83238bedc4c819cc866332cc951e8
matheus <matheusbritodasilva@gmail.com>
2019-10-21 14:47:03 -0300

    Listando repositórios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 import api from '../../services/api';
 // import { Container } from './styles';
 
@@ -31,7 +31,7 @@ export default class Main extends Component {
     };
 
     render() {
-        const { newRepo, loading } = this.state;
+        const { newRepo, loading, repositories } = this.state;
         return (
             <Container>
                 <h1>
@@ -54,6 +54,14 @@ export default class Main extends Component {
                         )}
                     </SubmitButton>
                 </Form>
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <span>{repository.name}</span>
+                            <a href="#">Detalhes</a>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
     }
